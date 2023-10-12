7a26f142d83f6f99083ff72c714ba32284571666
abdelra7man <abdelra7manabdullah@gmail.com>
2023-10-12 21:18:02 +0300

    feat(categories): Add Categories Model, Controller & Factory

--- resources/views/components/main-header-categories.blade.php
+++ resources/views/components/main-header-categories.blade.php
@@ -1,16 +1,11 @@
 <div class="col-md-3">
     <nav class="navbar bg-light">
         <ul class="navbar-nav">
-            {{--TODO:: Add a loop here--}}
             {{--Start Category List Item--}}
-            <x-main-header-category-list-item name="Best Selling" />
+                @foreach($categories as $category)
+                    <x-main-header-category-list-item :name="$category->name" />
+                @endforeach
             {{--End Category List Item--}}
-            <x-main-header-category-list-item name="New Arrivals" />
-            <x-main-header-category-list-item name="Fashion & Beauty" />
-            <x-main-header-category-list-item name="Kids & Babies Clothes" />
-            <x-main-header-category-list-item name="Men & Women Clothes" />
-            <x-main-header-category-list-item name="Gadgets & Accessories" />
-            <x-main-header-category-list-item name="Electronics & Accessories" />
         </ul>
     </nav>
 </div>
--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -1,7 +1,7 @@
 <div class="header">
     <div class="container-fluid">
         <div class="row">
-            <x-main-header-categories />
+            <x-main-header-categories :categories="$categories"/>
             <x-main-header-slider />
             <x-main-header-show-column />
         </div>
