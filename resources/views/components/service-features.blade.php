bec29358e363dff94e45ca6772061485de3acb04
abdelra7man <abdelra7manabdullah@gmail.com>
2023-10-12 20:24:19 +0300

    feat(index): Add Content To Home Page

--- resources/views/components/service-features.blade.php
+++ resources/views/components/service-features.blade.php
@@ -3,11 +3,11 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <!-- One Feature Start-->
-            <x-one-feature name="Secure Payment" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fab fa-cc-mastercard"/>
+            <x-one-service-feature name="Secure Payment" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fab fa-cc-mastercard"/>
             <!-- One Feature End-->
-            <x-one-feature name="Worldwide Delivery" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-truck"/>
-            <x-one-feature name="90 Days Return" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-sync-alt"/>
-            <x-one-feature name="24/7 Support" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-comments"/>
+            <x-one-service-feature name="Worldwide Delivery" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-truck"/>
+            <x-one-service-feature name="90 Days Return" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-sync-alt"/>
+            <x-one-service-feature name="24/7 Support" desc="Lorem ipsum dolor sit amet consectetur elit" icon="fa fa-comments"/>
         </div>
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-
+    <x-main-header/>
+    <x-brands/>
+    <x-service-features/>
+    <x-slide-view/>
+    <x-call/>
+    <x-featured-products/>
+    <x-newsletter/>
+    <x-recent-products/>
+    <x-review-cards/>
 </x-layout>
