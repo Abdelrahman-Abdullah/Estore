f820652c2dd1b37c96eaeb563691da054d27ec95
abdelra7man <abdelra7manabdullah@gmail.com>
2023-11-12 21:07:49 +0200

    feat(api-payment): Adding Checkout Mehod With API Route

--- app/Http/Controllers/API/StripeController.php
+++ app/Http/Controllers/API/StripeController.php
@@ -3,19 +3,41 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\PaymentRequest;
+use App\Services\OrderService;
+use App\Services\StripeService;
 use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
 
 class StripeController extends Controller
 {
-    public function checkout()
+    public function __construct(protected StripeService $stripe, protected OrderService $order)
     {
+    }
+    public function checkout(PaymentRequest $request)
+    {
+        $cart = $request->cart;
+        Stripe::setApiKey(config('stripe.secret_key'));
+        $totalPrice = $this->stripe->fillTheProductsArray($cart)['totalPrice'];
+        $session = Session::create([
+            'line_items' => $this->stripe->fillTheProductsArray($cart)['products'],
+            'mode' => 'payment',
+            'success_url' => route('checkout.success'),
+            'cancel_url' => route('checkout.cancel'),
+        ]);
+        $this->order->storeOrder($totalPrice, $session->id);
+        redirect($session->url);
     }
 
     public function success()
     {
+
     }
 
     public function cancel()
     {
+
     }
 }
