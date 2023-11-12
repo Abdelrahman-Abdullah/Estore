fc47eb5b54187c5b31896c0ce955f79fd21efb88
abdelra7man <abdelra7manabdullah@gmail.com>
2023-11-12 20:52:31 +0200

    feat(api-payment): Adding Stripe Controller Skeleton

---
+++ app/Http/Controllers/API/StripeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StripeController extends Controller
+{
+    public function checkout()
+    {
+    }
+
+    public function success()
+    {
+    }
+
+    public function cancel()
+    {
+    }
+}
